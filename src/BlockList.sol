b94e3e802983fb17bc330eedb2c777f9568b9b78
koloz <zach.kolodny@gmail.com>
2023-01-19 12:35:05 -0500

    added interface and registry for mixin

--- src/BlockList.sol
+++ src/BlockList.sol
@@ -1,9 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-
-/// @title BlockList
-/// @notice abstract contract that can be inherited to block
-///         approvals from non-royalty paying marketplaces
-/// @author transientlabs.xyz
+pragma solidity 0.8.17;
 
 /**
     ____        _ __    __   ____  _ ________                     __ 
@@ -14,102 +10,86 @@
                                                                     
 */
 
-pragma solidity 0.8.17;
+import {Initializable} from "openzeppelin-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableAccessControlUpgradeable} from "tl-sol-tools/upgradeable/access/OwnableAccessControlUpgradeable.sol";
+import {IBlockListRegistry} from "./IBlockListRegistry.sol";
 
-///////////////////// CUSTOM ERRORS /////////////////////
+/// @title BlockList
+/// @notice abstract contract that can be inherited to block
+///         approvals from non-royalty paying marketplaces
+/// @author transientlabs.xyz
+abstract contract BlockList is Initializable, OwnableAccessControlUpgradeable {
 
-/// @dev blocked operator error
-error BlockedOperator();
+    /*//////////////////////////////////////////////////////////////////////////
+                                Public State Variables
+    //////////////////////////////////////////////////////////////////////////*/
 
-///////////////////// BLOCKLIST CONTRACT /////////////////////
+    IBlockListRegistry public blockListRegistry;
 
-abstract contract BlockList {
+    /*//////////////////////////////////////////////////////////////////////////
+                                Events
+    //////////////////////////////////////////////////////////////////////////*/
 
-    ///////////////////// STATE VARIABLES /////////////////////
+    event BlockListRegistryUpdated(
+        address indexed oldRegistry,
+        address indexed newRegistry
+    );
 
-    address public blockListRegistry;
+    /*//////////////////////////////////////////////////////////////////////////
+                                Custom Errors
+    //////////////////////////////////////////////////////////////////////////*/
 
-    uint256 private _c; // variable that allows reset for `_blockList`
-    address public blockListOwner;
-    mapping(uint256 => mapping(address => bool)) private _blockList;
+    /// @dev blocked operator error
+    error BlockedOperator();
 
-    //================= Events =================//
-    event BlockListStatusChange(address indexed user, address indexed operator, bool indexed status);
-    event BlockListCleared(address indexed user);
-    event BlockListOwnershipTransferred(address indexed oldOwner, address indexed newOwner);
+    /*//////////////////////////////////////////////////////////////////////////
+                                Modifiers
+    //////////////////////////////////////////////////////////////////////////*/
 
-    //================= Modifiers =================//
-    /// @dev modifier to gate setter functions
-    modifier isBlockListOwner {
-        if (msg.sender != blockListOwner) {
-            revert NotBlockListOwner();
-        }
-        _;
-    }
     /// @dev modifier that can be applied to approval functions in order to block listings on marketplaces
     modifier notBlocked(address operator) {
-        if (_getBlockListStatus(operator)) {
-            revert IsBlockedOperator();
+        if (getBlockListStatus(operator)) {
+            revert BlockedOperator();
         }
         _;
     }
 
-    //================= Constructor =================//
-    constructor(address initBlockListOwner) {
-        _setBlockListOwner(initBlockListOwner);
+    /*//////////////////////////////////////////////////////////////////////////
+                                Initializer
+    //////////////////////////////////////////////////////////////////////////*/
+    
+    function __BlockList_init(address blockListRegistryAddr)
+        internal
+        onlyInitializing
+    {
+        blockListRegistry = IBlockListRegistry(blockListRegistryAddr);
     }
 
-    //================= Ownership Functions =================//
+    /*//////////////////////////////////////////////////////////////////////////
+                            Admin Functions
+    //////////////////////////////////////////////////////////////////////////*/
+
     /// @notice function to transfer ownership of the blockList
     /// @dev requires blockList owner
     /// @dev can be transferred to the ZERO_ADDRESS if desired
     /// @dev BE VERY CAREFUL USING THIS
-    function transferBlockListOwnership(address newBlockListOwner) public isBlockListOwner {
-        _setBlockListOwner(newBlockListOwner);
+    function updateBlockListRegistry(address newBlockListRegistry)
+        public
+        onlyOwner
+    {
+        blockListRegistry = IBlockListRegistry(newBlockListRegistry);
     }
 
-    //================= View Function =================//
+    /*//////////////////////////////////////////////////////////////////////////
+                          Public Read Functions
+    //////////////////////////////////////////////////////////////////////////*/
+
     /// @notice function to get blocklist status with True meaning that the operator is blocked
     function getBlockListStatus(address operator) public view returns (bool) {
-        return _getBlockListStatus(operator);
-    }
-
-    //================= Setter Functions =================//
-    /// @notice function to set the block list status for multiple operators
-    /// @dev must be called by the blockList owner
-    /// @dev the blockList owner is likely the same as the owner of the token contract 
-    ///      but this could be different under certain applications
-    function setBlockListStatus(address[] calldata operators, bool status) external isBlockListOwner {
-        for (uint256 i = 0; i < operators.length; i++) {
-            _setBlockListStatus(operators[i], status);
+        try blockListRegistry.getBlockListStatus(operator) returns (bool isBlocked) {
+          return isBlocked;
+        } catch {
+          return false;
         }
-    }
-
-    /// @notice function to clear the block list status
-    /// @dev must be called by the blockList owner
-    /// @dev the blockList owner is likely the same as the owner of the token contract 
-    ///      but this could be different under certain applications
-    function clearBlockList() external isBlockListOwner {
-        _c++;
-        emit BlockListCleared(msg.sender);
-    }
-
-    //================= Helper Functions =================//
-    /// @notice internal function to get blockList status
-    function _getBlockListStatus(address operator) internal view returns (bool) {
-        return _blockList[_c][operator];
-    }
-
-    /// @notice internal function to set blockList owner
-    function _setBlockListOwner(address newOwner) internal {
-        address oldOwner = blockListOwner;
-        blockListOwner = newOwner;
-        emit BlockListOwnershipTransferred(oldOwner, newOwner);
-    }
-
-    /// @notice internal function to set blockList status for one operator
-    function _setBlockListStatus(address operator, bool status) internal {
-        _blockList[_c][operator] = status;
-        emit BlockListStatusChange(msg.sender, operator, status);
     }
 }
--- src/BlockListRegistry.sol
+++ src/BlockListRegistry.sol
@@ -1,9 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-
-/// @title BlockList
-/// @notice abstract contract that can be inherited to block
-///         approvals from non-royalty paying marketplaces
-/// @author transientlabs.xyz
+pragma solidity 0.8.17;
 
 /**
     ____        _ __    __   ____  _ ________                     __ 
@@ -14,11 +10,122 @@
                                                                     
 */
 
-pragma solidity 0.8.17;
+import {OwnableAccessControlUpgradeable} from "tl-sol-tools/upgradeable/access/OwnableAccessControlUpgradeable.sol";
 
-///////////////////// CUSTOM ERRORS /////////////////////
+/// @title BlockList
+/// @notice abstract contract that can be inherited to block
+///         approvals from non-royalty paying marketplaces
+/// @author transientlabs.xyz
+contract BlockListRegistry is OwnableAccessControlUpgradeable {
 
-/// @dev zero address error
-error BlockedOperator();
+    /*//////////////////////////////////////////////////////////////////////////
+                               Private State Variables
+    //////////////////////////////////////////////////////////////////////////*/
 
-///////////////////// BLOCKLIST CONTRACT /////////////////////
+    uint256 private _c; // variable that allows reset for `_blockList`
+    mapping(uint256 => mapping(address => bool)) private _blockList;
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                Events
+    //////////////////////////////////////////////////////////////////////////*/
+    
+    event BlockListStatusChange(
+        address indexed user,
+        address indexed operator,
+        bool indexed status
+    );
+
+    event BlockListCleared(address indexed user);
+
+    event BlockListOwnershipTransferred(
+        address indexed oldOwner,
+        address indexed newOwner
+    );
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                Custom Errors
+    //////////////////////////////////////////////////////////////////////////*/
+
+    /// @dev not blocklist owner error
+    error NotBlockListOwner();
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                Modifiers
+    //////////////////////////////////////////////////////////////////////////*/
+
+    /// @dev modifier to gate setter functions
+    modifier isBlockListOwner() {
+        if (msg.sender != owner()) {
+            revert NotBlockListOwner();
+        }
+        _;
+    }
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                Constructor
+    //////////////////////////////////////////////////////////////////////////*/
+    function initialize(address[] memory initBlockList) external initializer {
+      for (uint8 i = 0; i < initBlockList.length; i++) _setBlockListStatus(initBlockList[i], true);
+      __OwnableAccessControl_init(msg.sender);
+    }
+
+    /*//////////////////////////////////////////////////////////////////////////
+                            Admin Functions
+    //////////////////////////////////////////////////////////////////////////*/
+
+    /// @notice function to clear the block list status
+    /// @dev must be called by the blockList owner
+    /// @dev the blockList owner is likely the same as the owner of the token contract
+    ///      but this could be different under certain applications
+    function clearBlockList() external isBlockListOwner {
+        _c++;
+        emit BlockListCleared(msg.sender);
+    }
+
+    /*//////////////////////////////////////////////////////////////////////////
+                          Public Read Functions
+    //////////////////////////////////////////////////////////////////////////*/
+
+    /// @notice function to get blocklist status with True meaning that the operator is blocked
+    function getBlockListStatus(address operator) public view returns (bool) {
+        return _getBlockListStatus(operator);
+    }
+
+    /*//////////////////////////////////////////////////////////////////////////
+                          Public Write Functions
+    //////////////////////////////////////////////////////////////////////////*/
+
+    /// @notice function to set the block list status for multiple operators
+    /// @dev must be called by the blockList owner
+    /// @dev the blockList owner is likely the same as the owner of the token contract
+    ///      but this could be different under certain applications
+    function setBlockListStatus(address[] calldata operators, bool status)
+        external
+        isBlockListOwner
+    {
+        for (uint256 i = 0; i < operators.length; i++) {
+            _setBlockListStatus(operators[i], status);
+        }
+    }
+
+    
+
+    /*//////////////////////////////////////////////////////////////////////////
+                            Internal Functions
+    //////////////////////////////////////////////////////////////////////////*/
+
+    /// @notice internal function to get blockList status
+    function _getBlockListStatus(address operator)
+        internal
+        view
+        returns (bool)
+    {
+        return _blockList[_c][operator];
+    }
+
+    /// @notice internal function to set blockList status for one operator
+    function _setBlockListStatus(address operator, bool status) internal {
+        _blockList[_c][operator] = status;
+        emit BlockListStatusChange(msg.sender, operator, status);
+    }
+}
